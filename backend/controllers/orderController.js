a66983f2b01c0409f92a95c43f5934172450248a
pawan-0101 <145849008+pawan-0101@users.noreply.github.com>
2025-11-10 18:50:19 +0530

    Update frontend URL for order placement

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -8,7 +8,7 @@ const stripe =new Stripe(process.env.STRIPE_SECRET_KEY)
 //placing user order from frontend
 const placeOrder=async (req,res) => {
    // const frontend_url="https://cravio-frontend.onrender.com";
-     const frontend_url="http://localhost:5173/"; // Change this to your frontend URL if needed
+     const frontend_url="https://cravio-frontend-xk1f.onrender.com"; // Change this to your frontend URL if needed
     try {
         const newOrder = new orderModel({
             userId:req.body.userId,
